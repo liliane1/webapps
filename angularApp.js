4e4a9f2a255deaab8a16db4a4d41a293956d0558
liliane1 <liliane.cuypers.u1761@student.hogent.be>
2016-12-16 12:09:39 +0100

    toevoeging email, commentsl en bio

--- angularApp.js
+++ angularApp.js
@@ -1,21 +1,3 @@
-/*door de combinatie van ui-router in var app en
-app.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	//configureren van een home state door gebruik te maken van
-	//$stateProvider en $urlRouterProvider
-		function($stateProvider, $urlRouterProvider){
-		$stateProvider
-			.state('home, {
-				url:'/home',
-				templateUrl: '/home.html',
-				controller:'MainCtrl'
-			});
-			//redirect niet gespecifieerde routen
-		urlRouterProvider.ohterwise('home');
-}]);
-worden de verschillende posts en tekstvakken niet getoond*/
-
 var app = angular.module('flapperNews', ['ui.router']);
 
 app.controller('MainCtrl', [
@@ -23,105 +5,86 @@ app.controller('MainCtrl', [
 	'posts',
 	'auth',
 	function($scope, posts, auth){
-		//$scope.test = 'Hello World';
 		$scope.isLoggedIn = auth.isLoggedIn;
 		$scope.posts = posts.posts;
-		/*$scope.posts =[
-			{title: 'post 1', upvotes : 5},
-			{title: 'post 2', upvotes : 2},
-			{title: 'post 3', upvotes : 15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}
-		];*/
 		$scope.addPost = function(){
 
-			alert("hi ");
-			//voorkomen dat er lege titels worden toegevoegd aan de lijst
 			if(!$scope.title || $scope.title ===""){
 					alert("empty title");
 					return;
 			}
-			//$scope.posts.push({title: 'A new post!', upvotes: 0});
-			//$scope.posts.push({
-			// $scope.
+
 			posts.create({
 				title: $scope.title,
 				link : $scope.link
 			});
-				//upvotes: 0});
-				//comments: [
-				//{author: 'Joe', body: 'Cool post!', upvotes:0},
-				//{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes:0}
-				//]
-			//leegmaken tekstvak nadat er op de submit knop geklikt is
+
 			$scope.title= '';
 			$scope.link = '';
 			};
-			//waarde van een titel verhogen met 1
+
 		$scope.incrementUpvotes = function(post){
-			//post.upvotes +=1;
-			post.upvote(post);
+
+			posts.upvote(post);
+
 		}
 	}]);
-//$http is een angular functie: service voor de query voor onze route posts
 app.factory('posts', ['$http','auth', function($http, auth){
-	//service body
 	var postFactory = {
 		posts: []
 	};
-	//angular copy method: create a deep copy of the returned data
-	//ophalen van de posts
+
 	postFactory.getAll = function(){
-		//success() method: laat ons toe to bind functions die
-		//die worden uitgevoerd wanneer de request terugkeert.
 		return $http.get('/posts').success(function(data){
-		//een lijst van posts wordt getoond, deze lijst wordt
-		//gekopieerd naar de client side posts postFactory.
+
 			angular.copy(data, postFactory.posts);
 		});
 	};
 	//aanmaken van nieuwe posts
 	postFactory.create = function(post) {
-		alert("call post method");
+		//alert("call post method");
 		var token = {headers:{
 			Authorization:'Bearer ' + auth.getToken()}
 		};
-		alert("token"+'Bearer ' + auth.getToken());
+		//alert("token"+'Bearer ' + auth.getToken());
 
 		return $http.post('/posts', post, token).success(function(data){
-			alert("sucess post method");
+		//	alert("sucess post method");
 			postFactory.posts.push(data);
-		}).error(function(err){alert("error post method" + err);});
+		}).error(function(err){
+			//alert("error post method" + err);
+		});
 	};
 
 	//upvoting post
 	postFactory.upvote = function(post){
 		//put method: to upvote post; call returns successfully, update local copy to reflect
 		//the changes.
-		return $http.put('/posts/'+ post._id + 'upvote', null, {headers:{
-			Authorization: 'Bearer' + auth.getToken()}
+		return $http.put('/posts/'+ post._id + '/upvote', null, {headers:{
+			Authorization: 'Bearer ' + auth.getToken()}
 		}).success(function(data){
-				post.upvotes +=1;
+				post.upvotes += 1;
 			});
 	};
 
 	//get post by id
 	postFactory.get = function(id){
-		return ($http.get('/posts/' + id).then(function(res){
+		return $http.get('/posts/' + id).then(function(res){
 			return res.data;
-		}));
+		});
 	};
 
 	//toevoegen van comment
 	postFactory.addComment = function(id, comment){
 		return $http.post('/posts/' + id + '/comments', comment,{headers: {
-			Authorization: 'Bearer' + auth.getToken()}});
+			Authorization: 'Bearer ' + auth.getToken()}});
 	};
+
 	//enable upvoting of comments
 	postFactory.upvoteComment = function(post, comment){
 	    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null,
-	          {headers: {Authorization: 'Bearer' +auth.getToken()}}).success(function(data){
-			comment.upvotes +=1;
+	          {headers: {Authorization: 'Bearer ' + auth.getToken()}}).success(function(data){
+			comment.upvotes += 1;
 		});
 	};
 
@@ -131,17 +94,13 @@ app.factory('posts', ['$http','auth', function($http, auth){
 app.config([
 	'$stateProvider',
 	'$urlRouterProvider',
-	//configureren van een home state door gebruik te maken van
-	//$stateProvider en $urlRouterProvider
 		function($stateProvider, $urlRouterProvider){
-//home page te tonen
+			//home.html te tonen
 		$stateProvider
 			.state('home', {
 				url:'/home',
 				templateUrl: '/home.html',
 				controller:'MainCtrl',
-				//resolve property: anytime 'home' state is entered,
-				//automatically query all posts from our backend before state actually finisched loading
 				resolve:{
 				   postPromise: ['posts', function(posts){
 					return posts.getAll();
@@ -149,13 +108,14 @@ app.config([
 				}
 			})
 			//posts.htlm te tonen
-			.state('posts', {
-				url: '/posts/{id}',
-				templateUrl: '/posts.htlm',
+			.state('postsComments', {
+				url: '/postsComments/:id',
+				templateUrl: '/posts.html',
 				controller : 'PostsCtrl',
 				resolve: {
 				  post: ['$stateParams', 'posts', function($stateParams, posts){
-					return posts.get($stateParams.id);
+						var rep = posts.get($stateParams.id);
+						return rep;
 					}]
 				}
 			})
@@ -181,41 +141,37 @@ app.config([
 					}
 				}]
 			});
-			//redirect niet gespecifieerde routen
+
 		$urlRouterProvider.otherwise('home');
-	}]
-);
+	}
+
+]);
 //stateParams wordt verwijderd en vervangen door post
 //stateParams wordt gebruikt om Id van een URL op te halen en laadde de desbetreffende post op
 app.controller('PostsCtrl', [
-		'scope',
+		'$scope',
 		'posts',
 		'post',
-		'auth',
-		function($scope, posts, post){
-			//$scope.post = posts.posts[$stateParams.id];
+	'auth',
+		function($scope, posts, post, auth){
 			$scope.isLoggedIn = auth.isLoggedIn;
 			$scope.post = post;
 		$scope.addComment = function(){
 			if($scope.body === '')
 			{return;}
-			$scope.addComment(post._id, {
+
+			posts.addComment(post._id, {
 			    body: $scope.body,
 			    author: 'user',}).success(function(comment){
 			     $scope.post.comments.push(comment);
 			   });
-			/*$scope.post.comments.push({
-				body: $scope.body,
-				author: 'user',
-				upvotes: 0
-				});*/
+
 			$scope.body = '';
 			};
-		$scope.incrementUpvote = function(comment){
+
+		$scope.incrementUpvotes = function(comment){
 			posts.upvoteComment(post, comment);
 		    };
-
-
 	}]
 );
 
@@ -250,8 +206,7 @@ app.factory('auth', ['$http', '$window', function($http, $window){
 			return payload.username;
 		}
 	};
-	//aanmaken van register functie om de gebruiker te posten met /register route
-	//en saves the token returned.
+
 	auth.register = function(user){
 	return $http.post('/register', user).success(function(data){
 		auth.saveToken(data.token);
@@ -279,10 +234,6 @@ app.controller('AuthCtrl', [
 	function($scope, $state, auth){
 		//initialize user op scope voor ons form
 		$scope.user = {};
-		//create register en logIn method on scope
-		//zo worden de respectievelijke methoden van auth.factory aangeroepen
-		//error gaat er voor zorgen dat de foutmelding op het scherm verschijnt
-		//geen errors dan gaat terug naar de home state
 		$scope.register = function(){
 			auth.register($scope.user).error(function(error){
 				$scope.error = error;
@@ -292,7 +243,7 @@ app.controller('AuthCtrl', [
 		};
 
 		$scope.logIn = function(){
-			auth.logIn($scoper.user).error(function(error){
+			auth.logIn($scope.user).error(function(error){
 				$scope.error = error;
 			}).then(function(){
 				$state.go('home');
