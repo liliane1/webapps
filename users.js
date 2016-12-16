7190616fc24990157e9fe767d03b6ae7d76ff970
liliane1 <liliane.cuypers.u1761@student.hogent.be>
2016-12-16 12:24:48 +0100

    toevoeging email en bio

--- users.js
+++ users.js
@@ -1,9 +1,50 @@
-var express = require('express');
-var router = express.Router();
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true},
+  bio: {type: String},
+  email: {type: String},
+
+  hash: String,
+  salt: String
 });
+/
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+//paswoord validatie
+UserSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 
-module.exports = router;
+  return this.hash === hash;
+};
+//aanmaken userschema email
+UserSchema.methods.setEmail = function(email){
+  //validatie van email
+  var testIFGoodMail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g;
+  if(email.match(testIFGoodMail)){
+    this.email = email;
+    return true;
+  }
+  return false;
+}
+
+UserSchema.methods.setBio = function(bio){
+    this.bio = bio;
+}
+
+UserSchema.methods.generateJWT = function(){
+  //set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+  return jwt.sign({
+    _id: this._id,
+    username : this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  },
+  'SECRET');
+};
+mongoose.model('User', UserSchema);
