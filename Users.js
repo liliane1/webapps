52740c3fc79ad3acdfaff18ee52cc70a3f3ff355
liliane1 <liliane.cuypers.u1761@student.hogent.be>
2016-12-16 12:02:19 +0100

    email en bio toegevoegd

--- Users.js
+++ Users.js
@@ -1,22 +1,17 @@
 var mongoose = require('mongoose');
-//paswoord onleesbaar
 var crypto = require('crypto');
-//regeneratie JWT token voor de gebruiker
 var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, unique: true},
-//opslag plaats voor paswoord
+  bio: {type: String},
+  email: {type: String},
+
   hash: String,
   salt: String
 });
-//aanmaken van setPassword method: hier wordt het paswoord aanvaardt
-UserSchema.methods.setPassword = function(password){
+/
   this.salt = crypto.randomBytes(16).toString('hex');
-  //pbkdf2(): wordt gebruikt om het paswoord onleesbaar te maken op het scherm
-  //de functie bevat: password, salt, iteratie en key length
-  //we moeten er voor opletten dat de iteratie en key length overeenkomt met
-  // de methode validPassword
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 //paswoord validatie
@@ -25,21 +20,31 @@ UserSchema.methods.validPassword = function(password){
 
   return this.hash === hash;
 };
-//
+//aanmaken userschema email
+UserSchema.methods.setEmail = function(email){
+  //validatie van email
+  var testIFGoodMail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g;
+  if(email.match(testIFGoodMail)){
+    this.email = email;
+    return true;
+  }
+  return false;
+}
+
+UserSchema.methods.setBio = function(bio){
+    this.bio = bio;
+}
+
 UserSchema.methods.generateJWT = function(){
   //set expiration to 60 days
   var today = new Date();
   var exp = new Date(today);
   exp.setDate(today.getDate() + 60);
-//jwt.sign method: payload dat wordt getekend.
-//zowel de server en de client hebben toegang tot de payload.
   return jwt.sign({
-    _id: this.id,
+    _id: this._id,
     username : this.username,
-    //exp: Unix tijdseenheid in seconde
     exp: parseInt(exp.getTime() / 1000),
   },
-  //wordt gebruikt om de token te tekenen
   'SECRET');
 };
 mongoose.model('User', UserSchema);
